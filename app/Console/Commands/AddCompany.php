bdb8239ebef1dc7c833abf49fdb9e8939b9b6998
DenEvtifeev <D.o.evtifeev@gmail.com>
2025-02-12 12:08:35 +0200

    Update AddCompany.php

--- app/Console/Commands/AddCompany.php
+++ app/Console/Commands/AddCompany.php
@@ -7,15 +7,35 @@ use App\Models\Company;
 
 class AddCompany extends Command
 {
+    /**
+     * Название команды.
+     *
+     * @var string
+     */
     protected $signature = 'add:company {name}';
+
+    /**
+     * Описание команды.
+     *
+     * @var string
+     */
     protected $description = 'Добавить новую компанию';
 
+    /**
+     * Выполняем логику команды.
+     *
+     * @return int
+     */
     public function handle()
     {
         $name = $this->argument('name');
 
-        $company = Company::create(['company_name' => $name]);
+        // Убедитесь, что в модели Company в $fillable присутствует 'name'
+        Company::create([
+            'name' => $name
+        ]);
 
-        $this->info("Компания '{$company->company_name}' успешно добавлена с ID {$company->id}");
+        $this->info("Компания '{$name}' успешно добавлена.");
+        return 0;
     }
 }
